cc4cd7c3d79b373f46dd610566cbc3b14a9fea7c
WHITEWAYS\JosephJ <joseph.jessop@uk.alpineracing.com>
2023-02-21 17:55:00 +0000

    1.1.1 Tube Inner Diameter Experiment - fucntion updates to plot all test cases

--- Funcs/Exp_1_1_TubeID.m
+++ Funcs/Exp_1_1_TubeID.m
@@ -1,21 +1,21 @@
 function PlotExp1()
     
-    raw_data{1} = xlsread( 'I:\JJ\09 - MATLAB\Pressure response analysis\SweepCode\Experimental Results\Hypodermic tube testing results.xlsx', '1.15mm' );
-    raw_data{2} = xlsread( 'I:\JJ\09 - MATLAB\Pressure response analysis\SweepCode\Experimental Results\Hypodermic tube testing results.xlsx', '1.35mm' );
-    raw_data{3} = xlsread( 'I:\JJ\09 - MATLAB\Pressure response analysis\SweepCode\Experimental Results\Hypodermic tube testing results.xlsx', '1.65mm' );
-    raw_data{4} = xlsread( 'I:\JJ\09 - MATLAB\Pressure response analysis\SweepCode\Experimental Results\Hypodermic tube testing results.xlsx', '2.45mm' );
-    sheets = sheetnames('I:\JJ\09 - MATLAB\Pressure response analysis\SweepCode\Experimental Results\Hypodermic tube testing results.xlsx');
+    raw_data{1} = xlsread( 'G:\01 - Aero Projects\06 - FS Data\Pressure Response Anaysis\1 - Experimental Tests\1.1 - Tube Properties\1.1.1 - Tube Inner Diameter\Hypodermic tube testing results.xlsx', '0.68mm' );
+    raw_data{2} = xlsread( 'G:\01 - Aero Projects\06 - FS Data\Pressure Response Anaysis\1 - Experimental Tests\1.1 - Tube Properties\1.1.1 - Tube Inner Diameter\Hypodermic tube testing results.xlsx', '1.12mm' );
+    raw_data{3} = xlsread( 'G:\01 - Aero Projects\06 - FS Data\Pressure Response Anaysis\1 - Experimental Tests\1.1 - Tube Properties\1.1.1 - Tube Inner Diameter\Hypodermic tube testing results.xlsx', '1.37mm' );
+    raw_data{4} = xlsread( 'G:\01 - Aero Projects\06 - FS Data\Pressure Response Anaysis\1 - Experimental Tests\1.1 - Tube Properties\1.1.1 - Tube Inner Diameter\Hypodermic tube testing results.xlsx', '2.058mm' );
+    sheets = sheetnames('G:\01 - Aero Projects\06 - FS Data\Pressure Response Anaysis\1 - Experimental Tests\1.1 - Tube Properties\1.1.1 - Tube Inner Diameter\Hypodermic tube testing results.xlsx');
     
     experiment_1_response = struct();
 
-    for i = 1:numel(raw_data)
+    for i = 1:length(raw_data)
         experiment_1_response(i).amp = raw_data{i}(1:end,3);
         experiment_1_response(i).phase = raw_data{i}(1:end,5)*-1;
         experiment_1_response(i).frequency = raw_data{i}(1:end,1);
         experiment_1_response(i).tube_radius = sheets(i);
     end
 
-    for j = 3
+    for j = i = 1:length(raw_data)
 %         figure;
         ax1 = subplot(2,1,1);
         plot(ax1,experiment_1_response(j).frequency, experiment_1_response(j).amp);
