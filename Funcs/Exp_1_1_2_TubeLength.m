5d756352414d1554b7c01715bf66370701dc1f90
WHITEWAYS\JosephJ <joseph.jessop@uk.alpineracing.com>
2023-02-21 18:07:08 +0000

    1.1.2 Tube Length Experiment - fucntion creation

---
+++ Funcs/Exp_1_1_2_TubeLength.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Exp_1_1_2_TubeLength(inputArg1,inputArg2)
+%EXP_1_1_2_TUBELENGTH Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
